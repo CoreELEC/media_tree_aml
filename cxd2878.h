fdeabdf17b9a87f98e60e97920060a553960910d
Davin622 <kodongling@hotmail.com>
2022-06-17 17:47:22 +0800

    add support for tbs5230 usb box

--- cxd2878.h
+++ cxd2878.h
@@ -45,6 +45,18 @@ struct cxd2878_config{
             - 1: Continuous (Default)
     */
     u8 ts_clk;
+	/**
+	 @brief Disable/Enable TS clock during specified TS region.
+	
+			bit flags: ( can be bitwise ORed )
+			- 0 : Always Active
+			- 1 : Disable during TS packet gap (default)
+			- 2 : Disable during TS parity (default)
+			- 4 : Disable during TS payload
+			- 8 : Disable during TS header
+			- 16: Disable during TS sync
+	*/
+    u8 ts_clk_mask;
 		/**
 		 @brief Disable/Enable TSVALID during specified TS region.
 		
@@ -59,6 +71,8 @@ struct cxd2878_config{
 	u8 ts_valid;
 
 	u8 atscCoreDisable;
+	
+	bool lock_flag;  //for usb device 
 	//for ecp3 update
 	void (*write_properties) (struct i2c_adapter *i2c,u8 reg, u32 buf);
 	void (*read_properties) (struct i2c_adapter *i2c,u8 reg, u32 *buf);
